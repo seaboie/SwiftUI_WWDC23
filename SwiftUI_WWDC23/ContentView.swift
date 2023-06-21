6c9e385a4fe1d386d04fa5ebd0a947fb8162116d
kritbovorn <kritbovorn@gmail.com>
2023-06-21 21:59:10 +0700

    finish

--- SwiftUI_WWDC23/ContentView.swift
+++ SwiftUI_WWDC23/ContentView.swift
@@ -8,14 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    let colors: [Color] = [.red, .blue, .green, .yellow, .purple];
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Rectangle()
+                .fill(.green)
+            Rectangle()
+                .fill(.white)
+                .overlay(content: {
+                    GeometryReader { geo in
+                        let w = geo.size.width;
+                        (
+                            ScrollView(.horizontal) {
+                                LazyHStack(spacing: w * 0.05) {
+                                    ForEach(colors, id: \.self) { color in
+                                        RoundedRectangle(cornerRadius: 25)
+                                            .fill(color.gradient)
+                                            .frame(width: w * 0.75)
+                                    }
+                                }
+                                .padding(.horizontal, (w - (w * 0.75)) / 2)
+                                .scrollTargetLayout()
+                            }
+                                .scrollTargetBehavior(.viewAligned)
+                            
+                        )
+                        
+                    }
+                    
+                })
+            Rectangle()
+                .fill(.blue)
         }
-        .padding()
     }
 }
 
