2391ea2e7a031a28624bf32d0aa0411fadef692e
kritbovorn <kritbovorn@gmail.com>
2023-06-21 12:08:00 +0700

    Initial Commit

---
+++ SwiftUI_WWDC23/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI_WWDC23
+//
+//  Created by krit on 21/6/2566 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI_WWDC23/SwiftUI_WWDC23App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WWDC23App.swift
+//  SwiftUI_WWDC23
+//
+//  Created by krit on 21/6/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WWDC23App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
