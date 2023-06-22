e7eef78bdbfa54e1ee81aabb286f85fe36d45a1e
kritbovorn <kritbovorn@gmail.com>
2023-06-22 11:22:05 +0700

    finish

--- SwiftUI_WWDC23/ContentView.swift
+++ SwiftUI_WWDC23/ContentView.swift
@@ -8,14 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var showView: Bool = false;
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            if showView {
+                Rectangle()
+                    .fill(.red.gradient)
+                    .frame(width: 150, height: 150)
+                    .transition(MyTransition())
+                    
+            }
+            
+            Button("Show View") {
+                withAnimation {
+                    showView.toggle();
+                }
+            }
         }
-        .padding()
+    }
+}
+
+struct MyTransition: Transition {
+    func body(content: Content, phase: TransitionPhase) -> some View {
+         content
+            
+            .rotation3DEffect(
+                .init(degrees: phase.value * (phase == .willAppear ? 90 : -90)),
+                                      axis: (x: 1.0, y: 0.0, z: 0.0)
+            )
     }
 }
 
